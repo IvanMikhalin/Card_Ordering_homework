a29e1cefb396f8154c606b9bd0096d5fb5e3ac1d
Ivan Mikhalin <mikhalin_ivan@mail.ru>
2022-10-18 12:23:40 +0400

    Tests added

---
+++ src/test/java/CardOrderTest.java
@@ -0,0 +1,73 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CardOredTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver");
+    }
+
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldTestFormComplition() {
+        driver.get("http://localhost:9999/");
+        List<WebElement> inputs = driver.findElements(By.tagName("input"));
+        inputs.get(0).sendKeys("Иванов Иван");
+        inputs.get(1).sendKeys("+71112223344");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button__text")).click();
+        String expected = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldTestNameComplition() {
+        driver.get("http://localhost:9999/");
+        List<WebElement> inputs = driver.findElements(By.tagName("input"));
+        inputs.get(0).sendKeys("Ivanov Ivan");
+        inputs.get(1).sendKeys("+71112223344");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button__text")).click();
+        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        String actual = driver.findElement(By.className("input__sub")).getText();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldTestTeLFormComplition() {
+        driver.get("http://localhost:9999/");
+        List<WebElement> inputs = driver.findElements(By.tagName("input"));
+        inputs.get(0).sendKeys("Иванов Иван");
+        inputs.get(1).sendKeys("+7111222334");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button__text")).click();
+        List<WebElement> inputsSub = driver.findElements(By.tagName("span"));
+        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        String actual = inputsSub.get(10).getText();
+        assertEquals(expected, actual);
+    }
+}
